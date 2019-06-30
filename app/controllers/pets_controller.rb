ba7d6264249781a32f5aa9d4ef2264a306178235
Savannah C <sgcorrell@gmail.com>
2019-06-30 17:34:59 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -43,7 +43,6 @@ class PetsController < ApplicationController
       owner = Owner.create(name: params[:owner][:name])
       @pet.owner = owner
     end
-    
     @pet.save
     redirect to "pets/#{@pet.id}"
   end
