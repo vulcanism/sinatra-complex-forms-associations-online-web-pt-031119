6985334653c19a13b1a115708a7d0a37290ccfea
Savannah C <sgcorrell@gmail.com>
2019-06-30 17:32:58 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,13 +10,9 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-    @pet = Pet.create(name: params[:pet][:name])
-    if params[:pet][:owner_ids]
-      @pet.owner_id=params[:pet][:owner_ids].first
-    elsif params[:owner_name]
-      owner = Owner.create(name: params[:owner_name])
-      @pet.owner = owner
-    end
+
+    redirect to "pets/#{@pet.id}"
+  end
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
