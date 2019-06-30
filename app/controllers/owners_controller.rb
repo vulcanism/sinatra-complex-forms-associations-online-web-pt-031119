477e02b4f138b5ef29b95ae0a2f175e061a72e3f
Savannah C <sgcorrell@gmail.com>
2019-06-30 17:44:04 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -15,7 +15,7 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    redirect to "owners/#{@owner.id}/"
+    redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
@@ -40,7 +40,7 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    redirect to "owners/#{@owner.id}/"
+    redirect "owners/#{@owner.id}"
   end
   
 end
